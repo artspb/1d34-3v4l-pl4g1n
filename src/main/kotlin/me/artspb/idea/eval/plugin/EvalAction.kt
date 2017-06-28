9b92fa44f481ecf77bc277a75357c8e2bb93e2fc
Artem Khvastunov <Artem.Khvastunov@jetbrains.com>
2017-06-28 15:03:51 +0200

    make action work during a dumb mode

--- src/main/kotlin/me/artspb/idea/eval/plugin/EvalAction.kt
+++ src/main/kotlin/me/artspb/idea/eval/plugin/EvalAction.kt
@@ -4,12 +4,13 @@ import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
 import org.apache.http.client.entity.UrlEncodedFormEntity
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.message.BasicNameValuePair
 
-class EvalAction : AnAction() {
+class EvalAction : AnAction(), DumbAware {
     override fun actionPerformed(e: AnActionEvent) {
         val file = e.dataContext.getData(CommonDataKeys.VIRTUAL_FILE)
         if (file != null) {
